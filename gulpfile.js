da1c1e96a4ffd1c567c59730bc0955248ec8abac
Deliora90 <deliora90@gmail.com>
2021-07-14 16:38:00 +0300

    Add About product panel

--- gulpfile.js
+++ gulpfile.js
@@ -147,9 +147,9 @@ gulp.task('otf2ttf', function () {
 
 function fontsStyle(params) {
 
-    let file_content = fs.readFileSync(source_folder + '/sass/_fonts.scss');
+    let file_content = fs.readFileSync(source_folder + '/sass/base/_fonts.scss');
     if (file_content == '') {
-        fs.writeFile(source_folder + '/sass/_fonts.scss', '', cb);
+        fs.writeFile(source_folder + '/sass/base/_fonts.scss', '', cb);
         return fs.readdir(path.build.fonts, function (err, items) {
             if (items) {
                 let c_fontname;
@@ -157,7 +157,7 @@ function fontsStyle(params) {
                     let fontname = items[i].split('.');
                     fontname = fontname[0];
                     if (c_fontname != fontname) {
-                        fs.appendFile(source_folder + '/sass/_fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+                        fs.appendFile(source_folder + '/sass/base/_fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
                     }
                     c_fontname = fontname;
                 }
